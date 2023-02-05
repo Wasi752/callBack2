f4dfa80e007c345ca367ec065fc48fc19863faf6
https://github.com/Wasi752 <osiur2@gmail.com>
2023-02-06 04:59:39 +0600

    Call Back

---
+++ copy.js
@@ -0,0 +1,6 @@
+function Kitab(bukhari){
+    console.log(100);
+    bukhari();
+ }
+ Kitab(console.log(70));
+ Kitab(()=> console.log(50));
---
+++ index.js
@@ -0,0 +1,6 @@
+function Kitab(bukhari){
+   console.log(100);
+   bukhari(20);
+}
+Kitab((x)=>console.log(70 + x));
+Kitab((x)=>console.log(7 * x));
